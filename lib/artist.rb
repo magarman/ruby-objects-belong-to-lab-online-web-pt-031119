5c156b21208c9622003cd53fde0ede858fb024f3
Mari Garman <garman.mariana@gmail.com>
2019-05-26 18:26:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,3 @@
 class Artist
-  
   attr_accessor :name
-  
-  def initialize(name)
-    @name = name
-  end 
-  
 end
-
-
-#class Song
- 
- # attr_accessor :title
- 
-  #def initialize(title)
-   # @title = title
-  #end
-#end
